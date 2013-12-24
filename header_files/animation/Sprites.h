a5fc8e37a85928c2b713f9ac4776136d57717436
unknown <pachristos@gmail.com>
2013-12-24 18:18:57 +0200

    Correct Tiles and introduce Sprites

---
+++ header_files/animation/Sprites.h
@@ -0,0 +1,25 @@
+#ifndef _SPRITES_H_
+#define _SPRITES_H_
+
+#include "../Objects.h"
+#include "AnimationFilm.h"
+#include "..\tiles\TileLayer.h"
+#include <assert.h>
+
+class Sprite {
+		byte frameNo;
+		Rect frameBox;
+		Dim x, y;
+		bool isVisible;
+		AnimationFilm* currFilm;
+		TileLayer* myLayer;
+		public:
+				void SetFrame (byte i);
+				byte GetFrame () const;
+				void SetVisibility (bool v);
+				bool IsVisible () const;
+				bool CollisionCheck (Sprite *s);
+				Sprite (Dim _x, Dim _y, AnimationFilm* film);
+};
+
+#endif
