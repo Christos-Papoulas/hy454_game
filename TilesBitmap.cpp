e7c01dcceb772175e7b1b1eb6054faf2618d2a5b
unknown <pachristos@gmail.com>
2013-12-29 19:30:50 +0200

    more beautiful

--- TilesBitmap.cpp
+++ TilesBitmap.cpp
@@ -7,16 +7,16 @@ TilesBitmap::TilesBitmap() {
 
 Bitmap TilesBitmap::LoadTiles() {
 	tiles = al_load_bitmap(TILES_MAP_PATH);
-	al_convert_mask_to_alpha(tiles, al_map_rgba(0, 0, 0, 0));
 	assert( tiles );
+	al_convert_mask_to_alpha(tiles, al_map_rgba(0, 0, 0, 0));
 	return tiles;
 }
 
 void TilesBitmap::PutTile(Bitmap at, Dim x, Dim y, Index tile) const {
 	assert(at);
 	if(tile) {
-		Dim tilex = TerrainUtils::TileX(tile);
-		Dim tiley = TerrainUtils::TileY(tile);
-		al_draw_bitmap_region(tiles, tilex, tiley, SCREEN_WINDOW_WIDTH, SCREEN_WINDOW_HEIGHT, x, y, NULL); // @todo the real values
+		Dim tilex = TerrainUtils::TileX2(tile);
+		Dim tiley = TerrainUtils::TileY2(tile);
+		al_draw_bitmap_region(tiles, tilex, tiley, 16, 16, x*16, y*16, NULL); // @todo the real values
 	}
 }
