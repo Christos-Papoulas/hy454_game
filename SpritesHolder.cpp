2d55a14aaaca648db54bcb79e09dc46622cf42a8
unknown <pachristos@gmail.com>
2013-12-31 15:26:30 +0200

    first try to display mario

---
+++ SpritesHolder.cpp
@@ -0,0 +1,11 @@
+#include "header_files\mario\SpritesHolder.h"
+
+SpritesHolder::SpritesHolder() {
+		Sprite *spriteMario = new Sprite(100, 100, AnimationFilmHolder::GetFilm( std::string("mariowalking") ));
+		spriteMario->Display(AnimationFilmHolder::GetFilm( "mariowalking" )->GetBitmap());
+		
+}
+
+SpritesHolder::~SpritesHolder() {
+		;
+}
---
+++ header_files/mario/SpritesHolder.h
@@ -0,0 +1,16 @@
+#ifndef _SPRITES_HOLDER_H_
+#define _SPRITES_HOLDER_H_
+
+#include "../animation/Sprites.h"
+#include "../animation/AnimationFilmHolder.h"
+#include "../terrain/terrain.h"
+
+#include <string>
+
+class SpritesHolder {
+		public:
+				SpritesHolder();
+				~SpritesHolder();
+};
+
+#endif
